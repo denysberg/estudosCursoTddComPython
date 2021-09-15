d8ef78df9cba04b4616af583ebe8eb7423899b89
Denys Carvalho <170140393@aluno.unb.br>
2021-09-15 10:16:14 -0300

    finalização do curso

--- src/tests/test_usuario.py
+++ src/tests/test_usuario.py
@@ -5,13 +5,13 @@ import pytest
 from src.leilao.excecoes import LanceInvalido
 
 
-class Vini:
+class Artur:
     pass
 
 
 @pytest.fixture
 def artur():
-    return Usuario(Artur, 100.0)
+    return Usuario(artur, 100.0)
 
 
 @pytest.fixture
@@ -28,7 +28,7 @@ def test_deve_subtrair_valor_da_carteira_do_usuario_quando_este_propor_um_lance(
 def test_deve_permitir_propor_lance_quando_o_valor_eh_menor_que_o_valor_da_carteira(artur, leilao):
     artur.propoe_lance(leilao, 1.0)
 
-    assert vini.carteira == 99.0
+    assert artur.carteira == 99.0
 
 
 def test_deve_permitir_propor_lance_quando_o_valor_eh_igual_ao_valor_da_carteira(artur, leilao):
@@ -40,4 +40,4 @@ def test_deve_permitir_propor_lance_quando_o_valor_eh_igual_ao_valor_da_carteira
 def test_nao_deve_permitir_propor_lance_com_valor_maior_que_o_da_carteira(artur, leilao):
     with pytest.raises(LanceInvalido):
 
-        vini.propoe_lance(leilao, 200.0)
+        artur.propoe_lance(leilao, 200.0)
